a45b125200f4d03cf0db14dc7d9057d67089e9e2
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-01-21 20:25:28 -0300

    Created structured main

---
+++ main.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const fs = require('fs');
+
+
+function requestConfig(route = '', method = 'get') {
+    return {
+        method: method,
+        maxBodyLength: Infinity,
+        url: `https://rest.coinapi.io/v1/${route}`,
+        headers: { 
+          'Accept': 'application/json', 
+          'X-CoinAPI-Key': 'D23A0AE8-C582-485F-BB2B-CBAD4710FE27'
+        }
+    };
+}
+
+function request(config = {}, callback) {
+    axios(config)
+    .then((response) => {
+        callback(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+
+
+function main() {
+    request(
+        requestConfig('exchanges'),
+        function (response){
+            fs.writeFile(
+                'Exchanges.json',
+                JSON.stringify(response.data),
+                'utf8',
+                function(err) {
+                    if (err) throw err;
+                    console.log('complete');
+                }
+            );
+        }
+    );
+}
+
+if (require.main === module) {
+    main();
+}
