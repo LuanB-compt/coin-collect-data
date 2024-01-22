b35cc0b9fc235c1baa884cee92a751fcfbf90fa3
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-01-21 23:35:47 -0300

    Created sequelize instance to sqlite

---
+++ src/database/SQLite.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+function getDatabaseInstance(path = './out/db.sqlite') {
+    return new Sequelize({dialect: 'sqlite', storage: path});
+}
+
+const sequelize = getDatabaseInstance();
+
+module.exports = {sequelize};
