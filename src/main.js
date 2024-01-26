ea053217f15cf7ff83f3bb7118defed8e192dea8
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-01-26 19:32:13 -0300

    Created the logic to daily historical OHLC in main

--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
 const { Controller } = require("./controller/Controller");
+const { processerDateString } = require("./utils/helpers");
 
-function main(controller) {
-    controller.request(controller.createCallback);
+async function main() {
+    const controller = new Controller();
+    const count = await controller.getCount();
+    if (typeof count === "number"){
+        if (count == 0){
+            controller.request(controller.createCallback, '2023-01-01T00:00:00');
+            return false;
+        } else {
+            const lastObj = await controller.getLastObj();
+            if(lastObj == undefined) return true;
+            else {
+                var lastDate = JSON.stringify(lastObj.DateEnd);
+                lastDate = processerDateString(lastDate);
+                if (lastDate == '2024-01-20T00:00:00') return true;
+                else {
+                    controller.request(controller.createCallback, lastDate);
+                    return false;
+                }
+            }
+        }
+    } else {return true};
+}
+
+const start = async function() {
+    var finish = false;
+    while (finish == false) {
+        finish = await main();
+        console.log(finish);
+        setTimeout(() => {}, 5000);
+    };
 }
 
 if (require.main === module) {
-    const controller = new Controller();
-    main(controller);
+    start();
 }
