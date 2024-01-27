9bb9f88bcb4feb6740b6c264dd2d332f8e1061bb
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-01-27 15:05:00 -0300

    Created a data modeling to table in SQLite

---
+++ Historical/src/models/CoinTable.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Table, MetaData, Column, String, Double, DateTime, Integer
+
+metaObj = MetaData()
+
+CoinTableModel = Table(
+    "CoinTable",
+    metaObj,
+    Column("Symbol", String, nullable=False, autoincrement=False, unique=False),
+    Column("OpenDateTime", DateTime, nullable=False, autoincrement=False, unique=False),
+    Column("CloseDateTime", DateTime, nullable=False, autoincrement=False, unique=False),
+    Column("Open", Double, nullable=False, autoincrement=False, unique=False),
+    Column("High", Double, nullable=False, autoincrement=False, unique=False),
+    Column("Low", Double, nullable=False, autoincrement=False, unique=False),
+    Column("Close", Double, nullable=False, autoincrement=False, unique=False),
+    Column("Volume", Double, nullable=False, autoincrement=False, unique=False),
+    Column("NumTrades", Integer, nullable=False, autoincrement=False, unique=False)
+)
