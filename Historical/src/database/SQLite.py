49f45ae94e1ca44e01fdc8d0077fbfc5f77c16af
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-01-27 15:04:38 -0300

    Created a SQlite db instance

---
+++ Historical/src/database/SQLite.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, Engine
+
+def getDBinstance(path: str = "../out/db_.sqlite") -> Engine or False:
+    try:
+        return create_engine(url=f"sqlite:///{path}", echo=True)
+    except:
+        return False
+
+db = getDBinstance()
