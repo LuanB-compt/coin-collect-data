b6b1c3316cab210c54df2612f1cd09d321882abf
4Meloo <gabriel4melo@hotmail.com>
2024-02-17 00:14:21 -0500

    Created [Daily/controller]: method to save in database the result from request

--- Daily/src/controller/ControllerBinance.js
+++ Daily/src/controller/ControllerBinance.js
@@ -1,3 +1,5 @@
+const { CoinTableModel } = require("../models/CoinTable")
+const { sequelize } = require("../database/SQLite");
 const Binance = require('node-binance-api')
 
 
@@ -7,34 +9,49 @@ class ControllerBinance {
     binance = new Binance();
 
     constructor(symbol = '', interval = '15m'){
+        sequelize.sync()
         this.symbol = symbol;
         this.interval = interval
     }
 
     async request() {
-        this.binance.websockets.candlesticks([this.symbol], this.interval, (candlesticks) => {
-            let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks;
-            let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
-            const result = {
-                CloseTime: new Date(eventTime).toJSON(),
-                Open: open,
-                High: high,
-                Low: low,
-                Close: Close,
-                Volume: volume,
-                NumTrades: trades,
-                Symbol: this.symbol
-            }
+        await this.binance.websockets.candlesticks([this.symbol], this.interval, (candlesticks) => {
+            const { E:eventTime, k:ticks } = candlesticks;
+            const { o:open, h:high, l:low, c:close, v:volume, n:trades } = ticks;
             let endpoints = this.binance.websockets.subscriptions();
             for ( let endpoint in endpoints ) {
                 this.binance.websockets.terminate(endpoint);
             };
-            return result;
-        });
+            this.save({
+                CloseTime: new Date(eventTime).toJSON(),
+                Open: open,
+                High: high,
+                Low: low,
+                Close: close,
+                Volume: volume,
+                NumTrades: trades
+            })
+        })
+    }
+
+    async save(result){
+        let row = await CoinTableModel.create({
+            Symbol: this.symbol,
+            OpenDateTime: null,
+            CloseDateTime: result.CloseTime,
+            Open: result.Open,
+            High: result.High,
+            Low: result.Low,
+            Close: result.Close,
+            Volume: result.Volume,
+            NumTrades: result.NumTrades
+        })
+        row.save();
+        console.log("Created " + row.id + " ID");
     }
 }
 
 const c = new ControllerBinance("SOLUSDT");
-c.test();
+const result = c.request();
 
 module.exports = {ControllerBinance}
