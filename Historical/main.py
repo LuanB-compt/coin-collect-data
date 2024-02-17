15efef2fe37e50d416cf6d4acaa26c8e54f3871b
4Meloo <gabriel4melo@hotmail.com>
2024-02-16 20:24:00 -0500

    Updated [Historical/main]: USDT symbol

--- Historical/main.py
+++ Historical/main.py
@@ -4,6 +4,6 @@ from src.controller.Controller import Controller
 
 if __name__=="__main__":
     client = Client()
-    c = Controller(symbol="SOLBRL", client=client)
+    c = Controller(symbol="SOLUSDT", client=client)
     c.getOHLC()
     c.saveOHLC()
